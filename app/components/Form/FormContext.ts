a89b463fd97aa79b66c897dedcb310fce524de95
Ben <bdwells1996@gmail.com>
2026-03-05 10:26:33 +0000

    Feat: Form components and stories.

---
+++ app/components/Form/FormContext.ts
@@ -0,0 +1,15 @@
+import { createContext, useContext } from 'react'
+
+export interface FormContextValue {
+  errors: Record<string, string[] | undefined>
+  inForm: boolean
+}
+
+export const FormContext = createContext<FormContextValue>({
+  errors: {},
+  inForm: false,
+})
+
+export function useFormErrors(): FormContextValue {
+  return useContext(FormContext)
+}
---
+++ app/components/Form/index.ts
@@ -0,0 +1,3 @@
+export { Form } from './Form'
+export { useFormErrors } from './FormContext'
+export type { FormContextValue } from './FormContext'
