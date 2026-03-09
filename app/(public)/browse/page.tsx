79e4cefaead4ef0e0508086b14b83f5eb2825822
Ben <bdwells1996@gmail.com>
2026-03-09 15:50:55 +0000

    Fix: removed auth checl

--- app/(public)/browse/page.tsx
+++ app/(public)/browse/page.tsx
@@ -1,22 +1,15 @@
 import { Suspense } from "react";
 import { getRecipesByCategory } from "@/db/queries/recipes";
-import { auth } from "@/lib/auth";
 import { BrowseView } from "./components/BrowseView";
 
 export default async function BrowsePage() {
-  const [session, categories] = await Promise.all([
-    auth(),
-    getRecipesByCategory(),
-  ]);
+	const [categories] = await Promise.all([getRecipesByCategory()]);
 
-  return (
-    <div className="mx-auto max-w-6xl p-8">
-      <Suspense>
-        <BrowseView
-          categories={categories}
-          isAuthenticated={!!session?.user}
-        />
-      </Suspense>
-    </div>
-  );
+	return (
+		<div className="mx-auto max-w-6xl p-8">
+			<Suspense>
+				<BrowseView categories={categories} />
+			</Suspense>
+		</div>
+	);
 }
